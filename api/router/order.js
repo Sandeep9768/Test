05b921f433ed946cec78cb4549a0f35588c937bb
Prafful Jagtap <prafful.amexs@gmail.com>
2019-04-02 18:05:46 +0530

    final

--- api/router/order.js
+++ api/router/order.js
@@ -38,13 +38,13 @@ router.post('/order', function (req, res, next) {
       result
     ) {
       if (err) throw err
-
+      // res.status(err)
       result1.push(result)
 
       if (result1.length == length) {
         console.log(result, 'final')
 
-        res.json(true)
+        res.json(result)
       }
       //   console.log('Error', result)
       // res.send(result)
@@ -65,5 +65,73 @@ router.post('/order', function (req, res, next) {
   //     }
   //   })
 })
+
+router.post(
+  '/purchase',
+  function (req, res, next) {
+    // var date = new Date()
+    // console.log(req.body.id)
+
+    const sql = 'select * from Orders where id=?'
+    con.query(sql, [req.body.id], function (err, result) {
+      if (err) throw err
+
+      if (result.length > 0) {
+        // console.log(result, 'final')
+        req.body = []
+        req.body = result
+        console.log(req.body, 'body')
+        var final = []
+        var purchase = []
+        var data = req.body
+        data.map(user => {
+          console.log(user.product_id)
+          const sql = 'select * from Product where product_id=?'
+          con.query(sql, [user.product_id], function (err, result) {
+            if (err) throw err
+
+            if (result.length > 0) {
+              purchase.push(req.body)
+              purchase.push(result)
+              final.push(purchase)
+              res.send(final)
+              // res.json(purchase)
+            } else {
+              console.log('error')
+              res.json(err)
+            }
+          })
+        })
+        // res.json(result)
+        next()
+      } else {
+        console.log('error')
+        // res.json(err)
+      }
+    })
+  },
+  function (req, res, next) {
+    // var date = new Date()
+    // console.log(req.body, 'body')
+    // var purchase = []
+    // var data = req.body
+    // data.map(user => {
+    //   console.log(user.product_id)
+    //   const sql = 'select * from Product where product_id=?'
+    //   con.query(sql, [user.product_id], function (err, result) {
+    //     if (err) throw err
+    //     if (result.length > 0) {
+    //       req.body = result
+    //       purchase = req.body
+    //       res.send(purchase)
+    //       // res.json(purchase)
+    //     } else {
+    //       console.log('error')
+    //       res.json(err)
+    //     }
+    //   })
+    // })
+  }
+)
 
 module.exports = router
